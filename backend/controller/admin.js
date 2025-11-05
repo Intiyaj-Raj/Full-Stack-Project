81b9a447ee539318b34ab5d4a72d57b957f4b606
Intiyaj-Raj <intiyajansari8757@gmail.com>
2025-11-05 15:14:38 +0530

    Update admin.jsremove email and pass

--- backend/controller/admin.js
+++ backend/controller/admin.js
@@ -120,8 +120,8 @@ const mailReplyController = async (req, res) => {
             port: 587,
             secure: false,
             auth: {
-                user: "intiyajraj786@gmail.com",
-                pass: "drwh ckgz xkyn ggkb",
+                user: "intiyaj@gmail.com",
+                pass: "drwgkb",
             },
         });
 
