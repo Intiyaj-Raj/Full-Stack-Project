be93c3595f16de0a0d9f10edba55d7043a58cfc0
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-08-02 02:35:22 +0530

    deploy project on render

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const { model, Schema } = mongoose
 const userSchema = new Schema({
     userName: { type: String, require: true },
     userEmail: { type: String, require: true },
-    userPass: { type: String, require: true }
+    userPass: { type: String, require: true },
 })
 
 module.exports = model("user", userSchema) 
--- frontend_shopping/src/App.jsx
+++ frontend_shopping/src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from './pages/HomePage'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Query from './pages/Query'
-import Login from './components/Login'
-import Reg from './components/Reg'
-import Cart from './pages/Cart'
-import AdminDashboard from './admin/AdminDashboard'
-import AdminProducts from './admin/AdminProducts'
-import AddProducts from './admin/AddProducts'
-import EditProducts from './admin/EditProducts'
-import AdminQuery from './admin/AdminQuery'
-import QueryReply from './admin/QueryReply'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Query from "./pages/Query";
+import Login from "./components/Login";
+import Reg from "./components/Reg";
+import Cart from "./pages/Cart";
+import AdminDashboard from "./admin/AdminDashboard";
+import AdminProducts from "./admin/AdminProducts";
+import AddProducts from "./admin/AddProducts";
+import EditProducts from "./admin/EditProducts";
+import AdminQuery from "./admin/AdminQuery";
+import QueryReply from "./admin/QueryReply";
 
 const App = () => {
   return (
@@ -21,27 +20,26 @@ const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
-
           {/* for user */}
           <Route path="/" element={<HomePage />} />
           <Route path="/query" element={<Query />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/reg' element={<Reg />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reg" element={<Reg />} />
+          <Route path="/cart" element={<Cart />} />
 
           {/* for admin */}
 
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/adminproduct" element={<AdminProducts />} />
           <Route path="/admin/add-products" element={<AddProducts />} />
-          <Route path='/admin/edit-product/:id' element={<EditProducts />} />
-          <Route path='/admin/adminquery' element={<AdminQuery />} />
-          <Route path='/admin/queryreply/:id' element={<QueryReply />} />
+          <Route path="/admin/edit-product/:id" element={<EditProducts />} />
+          <Route path="/admin/adminquery" element={<AdminQuery />} />
+          <Route path="/admin/queryreply/:id" element={<QueryReply />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
